86cde132df14719d22ba6d0f67ddc947c8855916
hmud0721@naver.com <hmud0721@naver.com>
2024-10-01 21:55:47 +0900

    레벨 디자인

--- Assets/Scripts/WayPointMovement.cs
+++ Assets/Scripts/WayPointMovement.cs
@@ -9,7 +9,8 @@ public class WayPointMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
